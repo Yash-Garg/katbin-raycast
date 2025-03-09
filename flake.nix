4ee70ff990b02c29335669c0eddaa14531d3d52c
Yash-Garg <me@yashgarg.dev>
2025-03-09 13:51:59 +0530

    feat: command to create paste

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.devshell.inputs.nixpkgs.follows = "nixpkgs";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  inputs.flake-compat.url = "github:nix-community/flake-compat";
+  inputs.flake-compat.flake = false;
+
+  outputs =
+    {
+      nixpkgs,
+      devshell,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlays.default ];
+        };
+      in
+      {
+        devShells.default = pkgs.devshell.mkShell {
+          env = [
+            {
+              name = "DEVSHELL_NO_MOTD";
+              value = 1;
+            }
+          ];
+
+          packages = with pkgs; [
+            bun
+            just
+            nodejs_20
+          ];
+        };
+      }
+    );
+}
